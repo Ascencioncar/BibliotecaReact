9e749c0fad40dd26ba8b7e34af626375ad7a1ff8
Ascencioncar <carlosrp1918@gmail.com>
2024-10-16 04:51:06 -0500

    Cambie la forma de trabajar a expo

---
+++ screens/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Text,StyleSheet,View } from "react-native";
+
+export default function Home(){
+    return (
+        <View>
+            <Text>
+                
+            </Text>
+        </View>
+
+    );
+
+}
+
+const styles= StyleSheet.create({});
---
+++ screens/Login.js
@@ -0,0 +1,16 @@
+import React, {useState} from "react";
+import { Text,StyleSheet,View } from "react-native";
+
+export default function Login(){
+    return (
+        <View>
+            <Text>
+                
+            </Text>
+        </View>
+
+    );
+
+}
+
+const styles= StyleSheet.create({});
