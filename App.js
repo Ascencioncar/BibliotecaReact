ebd9340e5cd787a64141345eec248bf7f879caf5
Ascencioncar <carlosrp1918@gmail.com>
2024-10-16 05:32:30 -0500

    Interfaz de inicio agregada

--- App.js
+++ App.js
@@ -2,14 +2,39 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import 'react-native-gesture-handler';
 
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native'
+import Login from './screens/Login';
+import Home from './screens/Home';
+
 export default function App() {
 
-  
+  const Stack = createStackNavigator();
+
+  function MyStack() {
+    return (
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={Login} 
+        options={{
+          title: "Ingreso Biblioteca Online",
+          headerTintColor: 'white',
+          headerTitleAlign: 'center',
+          headerStyle: { backgroundColor: "#525FE1"}
+        }} />
+        <Stack.Screen name="Home" component={Home}
+        options={{title: "Inicio",
+          headerTintColor: 'white',
+          headerTitleAlign: 'center',
+          headerStyle: { backgroundColor: "#525FE1"}}}
+        />
+
+      </Stack.Navigator>
+    );
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <MyStack/>
+    </NavigationContainer>
   );
 }
 
--- screens/Login.js
+++ screens/Login.js
@@ -1,16 +1,83 @@
 import React, {useState} from "react";
-import { Text,StyleSheet,View } from "react-native";
+import { TextInput,StyleSheet,View, Image, TouchableOpacity,Text } from "react-native";
+
 
 export default function Login(){
     return (
-        <View>
-            <Text>
-                
-            </Text>
+        <View style={styles.padre}>
+            <View>
+                <Image source={require('../assets/inicio.png')} style={styles.profile}/>
+            </View>
+
+            <View style={styles.tarjeta}>
+                <View style={styles.cajaTexto}>
+                    <TextInput placeholder="Tu correo" style={styles.Correo}/>
+                </View>
+                <View style={styles.cajaTexto}>
+                    <TextInput placeholder="Contraseña" style={styles.Correo}/>
+                </View>
+
+                <View style={styles.PadreBoton}>
+                    <TouchableOpacity style={styles.cajaBoton}>
+                        <Text style={styles.TextoBoton}>Ingresar</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
         </View>
 
     );
 
 }
 
-const styles= StyleSheet.create({});
+const styles= StyleSheet.create({
+    padre:{
+        flex: 1,
+        justifyContent:'center',
+        alignItems:'center',
+        backgroundColor: 'white',
+    },
+    profile:{
+        width: 150,
+        height: 150,
+
+    },
+    tarjeta:{
+        margin: 20,
+        backgroundColor:'white',
+        borderRadius:20,
+        width:'90%',
+        padding:20,
+        shadowColor: '#000',
+        shadowOffset: {
+            width:0,
+            height:2,
+        },
+        shadowOpacity:0.25,
+        shadowRadius:4,
+        elevation:5,
+    },
+    Correo: {
+        paddingHorizontal: 15,
+    },
+    cajaTexto: {
+        paddingVertical: 20,
+        backgroundColor: '#cccccc40',
+        borderRadius: 30,
+        marginVertical: 10,
+    },
+    PadreBoton:{
+        alignItems:'center',
+    },
+    cajaBoton:{
+        backgroundColor: '#525FE1',
+        borderRadius: 30,
+        paddingVertical:20,
+        width: 150,
+        marginTop: 20
+    },
+    TextoBoton:{
+        textAlign: 'center',
+        color: 'white'
+    }
+
+});
